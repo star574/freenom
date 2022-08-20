4f6d5e9b872606851e535321e657e2b36c642a29
star574 <shihengluo574@gmail.com>
2022-08-20 22:19:36 +0800

    ~

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 wget https://raw.githubusercontent.com/star574/freenom/main/.env.example
 cp .env.example .env
 echo -e "FREENOM_PASSWORD='${{secrets.PASSWORD}}'\nFREENOM_USERNAME='${{secrets.USERNAME}}'" >> .env  
-cat .env
+cat .env | grep USERNAME
 docker run -d --name freenom  -v $(pwd):/conf -v $(pwd)/logs:/app/logs luolongfei/freenom
 sleep 10s
 docker logs freenom
